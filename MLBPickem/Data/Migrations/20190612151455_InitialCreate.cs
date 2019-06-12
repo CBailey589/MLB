675007e03d4be019ec1bfdb0d591e0a357e9b3ec
Connor Bailey <bailey.connor.p@gmail.com>
2019-06-12 10:22:26 -0500

    built models and DB

---
+++ MLBPickem/Data/Migrations/20190612151455_InitialCreate.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MLBPickem.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstPitchDateTime = table.Column<DateTime>(nullable: false),
+                    AwayTeamId = table.Column<int>(nullable: false),
+                    AwayLine = table.Column<string>(nullable: false),
+                    AwayStartingPitcher = table.Column<string>(nullable: true),
+                    HomeTeamId = table.Column<int>(nullable: false),
+                    HomeLine = table.Column<string>(nullable: false),
+                    HomeStartingPitcher = table.Column<string>(nullable: true),
+                    AwayScore = table.Column<int>(nullable: false),
+                    HomeScore = table.Column<int>(nullable: false),
+                    GameStarted = table.Column<bool>(nullable: false),
+                    GameComplete = table.Column<bool>(nullable: false),
+                    Inning = table.Column<string>(nullable: true),
+                    WinningTeamId = table.Column<int>(nullable: false),
+                    MLBScoreBoardId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: false),
+                    VIName = table.Column<string>(nullable: false),
+                    MLBName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGame",
+                columns: table => new
+                {
+                    UserGameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GameId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    ChosenTeamId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGame", x => x.UserGameId);
+                    table.ForeignKey(
+                        name: "FK_UserGame_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGame_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGame_GameId",
+                table: "UserGame",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGame_UserId",
+                table: "UserGame",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropTable(
+                name: "UserGame");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
