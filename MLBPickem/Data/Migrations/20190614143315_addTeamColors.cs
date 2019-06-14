ec6128c7d8dbd4d024473557c117e45625de847f
Connor Bailey <bailey.connor.p@gmail.com>
2019-06-14 10:44:03 -0500

    team colors/logos show up on Games/Index

---
+++ MLBPickem/Data/Migrations/20190614143315_addTeamColors.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MLBPickem.Data.Migrations
+{
+    public partial class addTeamColors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrimaryColor",
+                table: "Teams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryColor",
+                table: "Teams",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrimaryColor",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "SecondaryColor",
+                table: "Teams");
+        }
+    }
+}
