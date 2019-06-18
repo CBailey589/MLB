36c57e49d0d54d6abd30a8c7d12f32c8a726793c
Connor Bailey <bailey.connor.p@gmail.com>
2019-06-18 11:41:11 -0500

    Standings page shows Rank, Username, and Total Score

---
+++ MLBPickem/Models/ViewModels/UserScore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLBPickem.Models.ViewModels
+{
+    public class UserScore
+    {
+        public ApplicationUser User { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
